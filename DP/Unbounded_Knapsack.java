9ee4f452c3f3a3a623c13f795d017d1f1983125c
javeed999 <nicklen.453@gmail.com>
2025-06-05 18:52:24 +0530

    Create Unbounded_Knapsack.java

---
+++ DP/Unbounded_Knapsack.java
@@ -0,0 +1,115 @@
+capacity = 4
+profits = [10, 6, 1]
+weights = [3, 2, 1]
+ans:12
+
+  capacity = 10
+profits = [5, 11, 13]
+weights = [2, 4, 6]
+ans:27
+
+
+
+import java.util.*;
+ 
+public class Main {
+    static int helper(int i, int capacity, int[] profits, int[] weights) {
+        if (i == 0) return (capacity / weights[0]) * profits[0];
+        else if (capacity == 0) return 0;
+        int not_take = helper(i - 1, capacity, profits, weights);
+        int take = 0;
+        if (weights[i] <= capacity) take = profits[i] + helper(i, capacity - weights[i], profits, weights);
+        return Math.max(take, not_take);
+    }
+ 
+    static int unboundedKnapsack(int[] weights, int[] profits, int capacity) {
+        int n = weights.length;
+        return helper(n - 1, capacity, profits, weights);
+    }
+/*
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int capacity = sc.nextInt();
+        int[] profits = new int[n];
+        int[] weights = new int[n];
+        for (int i = 0; i < n; i++) profits[i] = sc.nextInt();
+        for (int i = 0; i < n;i++) weights[i] = sc.nextInt();
+        System.out.println(unboundedKnapsack(weights, profits, capacity));
+    }
+*/
+}
+
+///
+// Memorization
+import java.util.*;
+
+public class Main {
+    static int helper(int i, int capacity, int[] profits, int[] weights, int[][] memo) {
+        if (i == 0) return (capacity / weights[0]) * profits[0];
+        else if (capacity == 0) return 0;
+        if (memo[i][capacity] != -1) return memo[i][capacity];
+        int notTake = helper(i - 1, capacity, profits, weights, memo);
+        int take = 0;
+        if (weights[i] <= capacity) {
+            take = profits[i] + helper(i, capacity - weights[i], profits, weights, memo);
+        }
+        memo[i][capacity] = Math.max(take, notTake);
+        return memo[i][capacity];
+    }
+
+    static int unboundedKnapsack(int[] weights, int[] profits, int capacity) {
+        int n = weights.length;
+        int[][] memo = new int[n][capacity + 1];
+        for (int[] row : memo) Arrays.fill(row, -1);
+        return helper(n - 1, capacity, profits, weights, memo);
+    }
+
+/*
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int capacity = sc.nextInt();
+        int[] profits = new int[n];
+        int[] weights = new int[n];
+        for (int i = 0; i < n; i++) profits[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) weights[i] = sc.nextInt();
+        System.out.println(unboundedKnapsack(weights, profits, capacity));
+    }
+*/
+}
+
+import java.util.*;
+
+public class Main {
+    public static int unboundedKnapsack(int[] weights, int[] profits, int capacity) {
+        int n = weights.length;
+        int[][] dp = new int[n][capacity + 1];
+        for (int j = 0; j <= capacity; j++) {
+            dp[0][j] = (j / weights[0]) * profits[0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= capacity; j++) {
+                int not_take = dp[i - 1][j];
+                int take = 0;
+                if (weights[i] <= j) {
+                    take = profits[i] + dp[i][j - weights[i]];
+                }
+                dp[i][j] = Math.max(take, not_take);
+            }
+        }
+        return dp[n - 1][capacity];
+    }
+    /*
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int capacity = sc.nextInt();
+        int[] profits = new int[n];
+        int[] weights = new int[n];
+        for (int i = 0; i < n; i++) profits[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) weights[i] = sc.nextInt();
+        System.out.println(knapsack(weights, profits, capacity));
+    }
+    */
+}
