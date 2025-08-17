4cebdf353b89c6e86d67d28dd6dd1aaf182033a2
javeed999 <nicklen.453@gmail.com>
2025-08-17 22:13:10 +0530

    Update Repeated_Missing_Number.java

--- arrays/Repeated_Missing_Number.java
+++ arrays/Repeated_Missing_Number.java
@@ -1,3 +1,30 @@
+int arr[]= {3,1,2,5,3};
+		int n=arr.length;
+	    long sn=(n*(n+1))/2;
+        long s2n=( n * (n+1)* (2*n+1))/6;
+	    long s=0,s2=0;
+	    for(long num:arr)
+	    {
+	        s+=num;
+	        s2+=num*num;
+	    }
+	     long val1=s-sn; // x-y
+	     long val2=s2-s2n; //x2-y2
+	     val2=val2/val1;
+	     
+	     long dup=(val1+val2)/2;
+	     long mis=val2-dup;
+	     
+	     
+	     
+	    System.out.println(dup+" "+mis);
+
+
+
+
+
+
+
 /******************************************************************************
 
                             Online Java Compiler.
@@ -43,3 +70,71 @@ public class Main
 
 	}
 }
+
+
+
+
+
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 5, 4, 6, 7, 5};
+        int xor=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            xor^=arr[i];
+            xor^=(i+1);
+        }
+        
+        int bitNo=0;
+        while(true)
+        {
+            if( (xor & (1<< bitNo)) !=0)
+                break;
+            bitNo++;
+        }
+        
+        int one=0,zero=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if((arr[i] & (1<<bitNo) )!=0)
+                one=one^arr[i];
+            else 
+                zero=zero^ arr[i];
+        }
+        
+        
+        for(int i=1;i<=n;i++)
+        {
+            if((i & (1<<bitNo) )!=0)
+                one=one^i;
+            else 
+                zero=zero^i;
+        }
+        
+        
+        
+        System.out.println(one+" "+zero);
+        
+        
+	}
+}
+
+
+
+
+
+
+
+
+
